2f51806b0c02c0ea8adf3181b7a394ca1743fc6c
aleksStajic <sstajic10@gmail.com>
2022-01-25 18:08:57 -0800

    In Alarm V3, currently have settable alarm (on/off) and added flag to be complemented each time alarm is turned on/off

--- Lab2_Files/__a51.bat
+++ Lab2_Files/__a51.bat
@@ -1,5 +1,5 @@
 @echo off
 C:
-cd "\Users\sstaj\Desktop\UBC\ELEC291\Lab2\"
-"C:\Users\sstaj\Desktop\UBC\ELEC291\CrossIDE\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\sstaj\Desktop\UBC\ELEC291\Lab2\Alarm_Code_V2.asm"
-echo Crosside_Action Set_Hex_File C:\Users\sstaj\Desktop\UBC\ELEC291\Lab2\Alarm_Code_V2.HEX
+cd "\Users\sstaj\Desktop\UBC\ELEC291\ELEC291_Lab2\Lab2_Files\"
+"C:\Users\sstaj\Desktop\UBC\ELEC291\CrossIDE\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\sstaj\Desktop\UBC\ELEC291\ELEC291_Lab2\Lab2_Files\Alarm_Code_V3.asm"
+echo Crosside_Action Set_Hex_File C:\Users\sstaj\Desktop\UBC\ELEC291\ELEC291_Lab2\Lab2_Files\Alarm_Code_V3.HEX
